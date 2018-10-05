9a82b6ca787e5896300c9399facf0579287e89a0
Bob Ryan <rryan14@gmail.com>
2018-10-05 18:56:34 -0400

    Tweaked notification style

--- src/assets/jss/site-styles/components/snackbarStyle.jsx
+++ src/assets/jss/site-styles/components/snackbarStyle.jsx
@@ -60,6 +60,7 @@ const snackbarStyle = {
   },
   dark: {
     backgroundColor: "#313131",
+    opacity: ".9",
     color: "#ffffff",
     boxShadow:
       "0 12px 20px -10px rgba(255, 255, 255, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(255, 255, 255, 0.2)"
