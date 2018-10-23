0880fef7e7d5d8b16362bebd134b1af27fc452be
Bob Ryan <rryan14@gmail.com>
2018-10-23 15:59:23 -0400

    Detail form + coloring

--- src/components/Tables/PlantFarmDetail.jsx
+++ src/components/Tables/PlantFarmDetail.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import { Grid, GridColumn as Column, GridDetailRow } from '@progress/kendo-react-grid';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
+import Tooltip from "@material-ui/core/Tooltip";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
 
 import { orderBy } from '@progress/kendo-data-query';
 
@@ -27,6 +30,13 @@ function formatNum(value, decimals) {
     return newValue;
 }
 
+function formatMT(value, units) {
+    if (isNaN(value) || value <= 0)
+        return '-';
+    else
+        return value + units;
+}
+
 function hsl_col_perc(percent, start, end) {
     var a = percent / 100,
     b = (end - start) * a,
@@ -41,18 +51,18 @@ function colorMap(value) {
     let absValue = round(Math.abs(value),0);
 
     if (isNeg) {
-        if (absValue > 0 && absValue <= 25.0)
+        if (absValue >= 0 && absValue < 25)
             color = '#FFCC03';
-        else if (absValue > 25 && absValue <= 50)
+        else if (absValue >= 25 && absValue <= 50)
             color = '#F89C05';
-        else if (absValue > 50 && absValue <= 100)
+        else if (absValue > 50)
             color = '#ab0520';
     } else {
-        if (absValue > 0 && absValue <= 25)
+        if (absValue >= 0 && absValue <= 25)
             color = '#65B12F';
-        else if (absValue > 25 && absValue <= 50)
+        else if (absValue >= 25 && absValue <= 50)
             color = '#43A546';
-        else if (absValue > 50 && absValue <= 100)
+        else if (absValue > 50 )
             color = '#37611A';
     }
 
@@ -77,7 +87,7 @@ class cellPercentDiff extends React.Component {
         return (
             <td style={{padding: "4px"}}>
                 <div style={style}>
-                    {this.props.dataItem[this.props.field]}
+                    {round(this.props.dataItem[this.props.field], 1)}
                 </div>
             </td>
         );
@@ -124,6 +134,17 @@ class cellFloatRightColorize extends React.Component {
     }
 }
 
+class cellEllipsis extends React.Component {
+    render() {
+        const value = this.props.dataItem[this.props.field];
+        return (
+            <td style={{ width: 'auto', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'}} title={value}>
+                {value}
+            </td>
+        );
+    }
+}
+
 class DetailComponent extends GridDetailRow {
     constructor(props) {
         super(props);
@@ -147,37 +168,108 @@ class DetailComponent extends GridDetailRow {
 
             <section>
                 <Tabs value={this.state.tabIndex} fullWidth={true} color="white" onChange={this.handleTabChange}>
-                    <Tab label='Farm detail'> </Tab>
                     <Tab label='Plant Recap'/>
-                    <Tab label='Risk'/>
+                    <Tab label='Risk Analysis'/>
+                    <Tab label='Farm detail'/>
                 </Tabs>
 
+                {/* TAB 1 */}
                 { this.state.tabIndex === 0 &&
                     <div>
+                        <GridContainer>
+                            <GridItem md={2}>
+                                <br/>
+                                <div style={{textAlign: 'right'}}>
+                                    <p><strong>Total production (2017)</strong></p>
+                                    <p><strong>Projected (2018)</strong></p>
+                                    <p><strong>Shipped (2017)</strong> </p>
+                                    <p><strong>Shipped (2016)</strong></p>
+                                </div>
+                            </GridItem>
+                            <GridItem md={2}>
+                                <br/>
+                                <div style={{textAlign: 'left'}}>
+                                    <p> {dataItem.TotalProduction} {dataItem.TotalProduction > 0 ? ' MT' : '-'} </p>
+                                    <p> {dataItem.Projected2017} {dataItem.Projected2017 > 0 ? ' MT' : '-'}</p>
+                                    <p> {dataItem.Shipped2017} {dataItem.Shipped2017 > 0 ? ' MT' : '-'}</p>
+                                    <p> {dataItem.Shipped2016} {dataItem.Shipped2016 > 0 ? ' MT' : '-'}</p>
+
+                                </div>
+                            </GridItem>
+                            <GridItem md={2}>
+                                <br/>
+                                <div style={{textAlign: 'right'}}>
+                                    <p><strong>2 star production (2018):</strong></p>
+                                    <p><strong>Number of farms:</strong></p>
+                                </div>
+                            </GridItem>
+
+                            <GridItem md={2}>
+                                <br/>
+                                <div style={{textAlign: 'left'}}>
+                                    <p> {dataItem.Production2Star} {dataItem.Production2Star > 0 ? ' MT' : '-'} </p>
+                                    <p> {dataItem.Farms}</p>
+                                </div>
+                            </GridItem>
+                        </GridContainer>
+                    </div>
+                }
+
+                {/* TAB 2 */}
+                { this.state.tabIndex === 2 &&
+                    <div>
+                        <br/>
                         { dataItem.FarmData.map((item) =>
                             <p><strong style={{marginRight: "8px"}}>{item.BAPId}:</strong> {item.Name}, {item.Country}</p>
                         ) }
                     </div>
                 }
 
+                {/* TAB 1 */}
                 { this.state.tabIndex === 1 &&
                     <div>
-                        <p><strong>2017 - Total Production:</strong> {dataItem.TotalProduction} MT</p>
-                        <p><strong>2016 - Production (2 Star):</strong> {dataItem.Production2Star} MT</p>
-                        <p><strong>2016 - Shipped:</strong> {dataItem.Shipped2016} MT</p>
-                        <p><strong>2017 - Projected:</strong> {dataItem.Projected2017} MT</p>
-                        <p><strong>2017 - Shipped:</strong> {dataItem.Shipped2017} MT</p>
+                        <GridContainer>
+                            <GridItem md={3}>
+                                <br/>
+                                <div style={{textAlign: 'right'}}>
+                                    <p><strong>Production</strong></p>
+                                    <p><strong>Projected</strong></p>
+                                    <p><strong>Shortfall vs. surplus (#)</strong> </p>
+                                    <p><strong>Shortfall vs. surplus (%)</strong></p>
+                                </div>
+                            </GridItem>
+                            <GridItem md={2}>
+                                <br/>
+                                <div style={{textAlign: 'left'}}>
+                                    <p> {dataItem.TotalProduction} {dataItem.TotalProduction > 0 ? ' MT' : '-'} </p>
+                                    <p> {dataItem.Projected2017} {dataItem.Projected2017 > 0 ? ' MT' : '-'}</p>
+                                    <p> {dataItem.Delta} {dataItem.Delta > 0 ? ' MT' : '-'}</p>
+                                    <p> {dataItem.Risk} {dataItem.Risk > 0 ? '%' : '-'}</p>
+
+                                </div>
+                            </GridItem>
+                            <GridItem md={2}>
+                                <br/>
+                                <div style={{textAlign: 'right'}}>
+                                    <p><strong>Number of farms</strong></p>
+                                    <p><strong>Farm/plant ratio <i className={"fa fa-sm fa-info-circle"} title="The farm-to-plant ratio (1:1 is idea)"/>:</strong></p>
+                                    <p><strong>Average farm volume <i className={"fa fa-sm fa-info-circle"} title="Average farm volume is computed..."/>:</strong></p>
+                                    <p><strong>Adjusted farm volume <i className={"fa fa-sm fa-info-circle"} title="Farms that service more than plant have had their volumes adjusted"/>:</strong></p>
+                                </div>
+                            </GridItem>
+
+                            <GridItem md={2}>
+                                <br/>
+                                <div style={{textAlign: 'left'}}>
+                                    <p> {dataItem.Farms}</p>
+                                    <p> {dataItem.FarmPlantRatio}</p>
+                                    <p> {dataItem.AvgFarm} {dataItem.AvgFarm > 0 ? ' MT' : '-'}</p>
+                                    <p> {dataItem.AdjFarm} {dataItem.AdjFarm > 0 ? ' MT' : '-'}</p>
+                                </div>
+                            </GridItem>
+                        </GridContainer>
                     </div>
                 }
-
-                { this.state.tabIndex === 2 &&
-                    <div>
-                        there
-                    </div>
-
-                }
-
-
             </section>
         );
     }
@@ -205,9 +297,9 @@ class PlantFarmDetail extends React.Component {
             ths[i].setAttribute("style", "font-weight: 500; font-size: 14px");
 
             // For the last 4 columns, right justify the headings
-            if ( i > 3 && i <= 6 ) {
+            if ( i > 3 && i < 8 ) {
                 ths[i].setAttribute("style", "text-align: right; font-weight: 500; font-size: 14px;");
-            } else if ( i === 7 ) {
+            } else if ( i === 8 ) {
                 ths[i].setAttribute("style", "text-align: center; font-weight: 500; font-size: 14px;");
             }
         }
@@ -252,13 +344,13 @@ class PlantFarmDetail extends React.Component {
                     onExpandChange={this.expandChange}
                     >
                         <Column field="BAPId" title="BAP ID" minResizableWidth="110px" filterable={true}  />
-                        <Column field="Name" title="Plant Name" minResizableWidth="200px" width="300px"  />
-                        <Column field="Country" title="Country" />
+                        <Column field="Name" title="Plant Name" minResizableWidth="150px" width="200px" cell={cellEllipsis} />
+                        <Column field="Country" title="Country" width="100px" cell={cellEllipsis}/>
                         <Column field="Farms" title="Farms" type="number" cell={cellIntegerRight}/>
                         <Column field="TotalProduction" title="Total Production" type="number" cell={cellFloatRight}/>
                         <Column field="Projected2017" title="Total Projected" type="number"  cell={cellFloatRight} />
-                        <Column field="Delta" title="Delta" type="number" cell={cellFloatRightColorize}  />
-                        <Column field="Percent" title="Risk %" type="number" cell={cellPercentDiff}  />
+                        <Column field="Delta" title="Difference" type="number" cell={cellFloatRightColorize}  />
+                        <Column field="Risk" title="Risk %" type="number" width="130px" cell={cellPercentDiff}  />
 
                         {/* <Column field="Production2Star" title="2 Star Production" format="{0:n01}" cell={cellWithBackGround}/>
                         <Column field="Shipped2016" title="2016 Shipped" format="{0:n01}" cell={cellWithBackGround}/>
