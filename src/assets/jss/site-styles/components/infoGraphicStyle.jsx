145cf3990d3f404efc00c1414ea5d0f9dffc87fc
Bob Ryan <rryan14@gmail.com>
2018-10-29 22:23:08 -0400

    Infographics using shared styles; changed round to integers

---
+++ src/assets/jss/site-styles/components/infoGraphicStyle.jsx
@@ -0,0 +1,56 @@
+const infoGraphicStyle = theme => ({
+    card: {
+        width: '100%',
+        background: "#539127",
+        height: "140px",
+        marginBottom: '20px'
+    },
+    heading: {
+        height: '22px',
+        textAlign: 'right',
+        color: '#fff',
+        fontSize: '18px',
+        fontWeight: '400',
+        marginRight: '15px',
+        marginTop: '15px'
+    },
+    title: {
+        color: '#fff',
+        fontSize: '42px',
+        textAlign:  'center',
+        margin: 0,
+        display: 'block',
+        paddingTop: '6px',
+
+    },
+    delta: {
+        fontSize: '13px',
+        color: '#fff',
+        marginLeft: '4px',
+        verticalAlign: 'middle',
+    },
+    arrows: {
+        marginLeft: '4px'
+    },
+    legend: {
+        fontSize: '14px',
+        color: '#fff',
+        margin: 0,
+        paddingTop: '6px',
+        display: 'block'
+    },
+    icon: {
+        paddingLeft: '12px'
+    },
+    tooltipCursor: {
+        cursor: 'pointer'
+    },
+    lightTooltip: {
+        background: theme.palette.common.white,
+        color: theme.palette.text.primary,
+        boxShadow: theme.shadows[1],
+        fontSize: 13,
+    },
+});
+
+export default infoGraphicStyle;
--- src/components/InfoGraphics/InfoGraphic2.jsx
+++ src/components/InfoGraphics/InfoGraphic2.jsx
@@ -1,101 +1,37 @@
 import React from "react";
-// @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
 
-// core components
 import GridContainer from "components/Grid/GridContainer.jsx";
 import GridItem from "components/Grid/GridItem.jsx";
-import StarIcon from "@material-ui/icons/StarRate";
-
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import Tooltip from '@material-ui/core/Tooltip';
 import ProductionIcon from "./../Icons/ProductionIcon.jsx";
+import Tooltip from '@material-ui/core/Tooltip';
+import infoGraphicStyle from "assets/jss/site-styles/components/infoGraphicStyle.jsx";
 
-function round(value, decimals) {
-    return Number(Math.round(value +'e'+ decimals) +'e-'+ decimals).toFixed(decimals);
-}
-
-const styles = theme => ({
-    card: {
-        width: '100%',
-        background: "#539127",
-        height: "140px",
-        marginBottom: '20px'
-    },
-    heading: {
-        height: '22px',
-        textAlign: 'right',
-        color: '#fff',
-        fontSize: '18px',
-        fontWeight: '400',
-        marginRight: '15px',
-        marginTop: '15px'
-    },
-    title: {
-        color: '#fff',
-        fontSize: '42px',
-        textAlign:  'center',
-        margin: 0,
-        display: 'block',
-        paddingTop: '6px',
-
-    },
-    delta: {
-        fontSize: '13px',
-        color: '#fff',
-        marginLeft: '4px',
-        verticalAlign: 'middle',
-    },
-    arrows: {
-        marginLeft: '4px'
-    },
-    legend: {
-        fontSize: '14px',
-        color: '#fff',
-        margin: 0,
-        paddingTop: '6px',
-        display: 'block'
-    },
-    icon: {
-        paddingLeft: '12px'
-    },
-    tooltipCursor: {
-        cursor: 'pointer'
-    },
-    lightTooltip: {
-        background: theme.palette.common.white,
-        color: theme.palette.text.primary,
-        boxShadow: theme.shadows[1],
-        fontSize: 13,
-    },
-});
-
+// function round(value, decimals) {
+//     return Number(Math.round(value +'e'+ decimals) +'e-'+ decimals).toFixed(decimals);
+// }
 
 class InfoGraphic2 extends React.Component {
     render() {
         const { classes } = this.props;
 
-        const production1 = {volume: 28.9, label: 2016};
-        const production2 = {volume: 24.9, label: 2017};
-        let delta = round( ((production2.volume - production1.volume) / production2.volume * 100), 1);
+        const production1 = {volume: 28.9, label: 2017};
+        const production2 = {volume: 24.9, label: 2018};
+        let delta = Math.round(((production2.volume - production1.volume) / production2.volume * 100));
 
         return (
             <div>
-                <Card className={classes.card} >
+                <Card className={classes.card} style={{backgroundColor: '#539127'}}>
                     <div className={classes.heading}>
                         Production (YoY) &nbsp;
 
-                         <Tooltip classes={{ tooltip: classes.lightTooltip }}
-                             title="The audited production volume between 2016 and 2017.">
-                             <span className={classes.tooltip}>
-                                 <i className={"fa fa-sm fa-info-circle"}/>
+                        <Tooltip classes={{ tooltip: classes.lightTooltip }}
+                            title="The audited production volume between 2016 and 2017.">
+                            <span className={classes.tooltip}>
+                                <i className={"fa fa-sm fa-info-circle"}/>
                             </span>
                         </Tooltip>
                     </div>
@@ -112,7 +48,7 @@ class InfoGraphic2 extends React.Component {
                                 <GridItem md={4} className={classes.title}>
                                     {production2.volume}
 
-                                    <label className={classes.delta}>{delta}
+                                    <label className={classes.delta}>{delta}%
                                         <span className={classes.arrows}>
                                             { delta < 0 && <i className={"fa fa-md fa-arrow-down"} title="Decrease"/>}
                                             { delta > 0 && <i className={"fa fa-md fa-arrow-up"} title="Increase"/>}
@@ -130,4 +66,4 @@ class InfoGraphic2 extends React.Component {
     }
 }
 
-export default withStyles(styles)(InfoGraphic2);
+export default withStyles(infoGraphicStyle)(InfoGraphic2);
